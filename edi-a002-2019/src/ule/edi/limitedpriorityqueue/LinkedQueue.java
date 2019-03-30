5358f2dc59a645c208ccbb7250099f956814bfa2
Alejandro Pérez Gancedo <jandrito97@gmail.com>
2019-03-30 21:57:43 +0100

    LinkedQueue Fixed

--- edi-a002-2019/src/ule/edi/limitedpriorityqueue/LinkedQueue.java
+++ edi-a002-2019/src/ule/edi/limitedpriorityqueue/LinkedQueue.java
@@ -1,119 +1,131 @@
 package ule.edi.limitedpriorityqueue;
 
 public class LinkedQueue<T> implements QueueADT<T> {
-	
+
 	protected static class Node<D> {
 		D element;
 		Node<D> next;
-		
+
 		Node() {
-		this.element = null;
-		this.next = null;
+			this.element = null;
+			this.next = null;
 		}
 		Node(D element) {
-		this.element = element;
-		this.next = null;
+			this.element = element;
+			this.next = null;
 		}
-		
-		}
+
+	}
 
 	private int count;
 	private Node<T> front, rear; 
-	
+
 	public LinkedQueue()
-	 {
-		
-		
+	{
+
+
 		count = 0;
 		front = null;
 		rear= null;
-		
-	 } 
-	
+
+	} 
+
 	@Override
 	public void enqueue(T element) {
-		
+		Node<T> actual = front;
 		Node<T> nodo = new Node<T>(element);
-		if (isEmpty()) {
-			front = rear= nodo;
-		}else {
-			
-			rear.next= nodo;
-			rear = nodo;
+
+		if (actual==null) {
+			front = nodo;
+			nodo.next=rear;
 		}
-		count ++;
-		
-	 
+		while (actual!= rear){
+			if (actual.next==rear) {
+				nodo.next = rear;
+				actual.next = nodo;
+			}
+			actual = actual.next;
+		}
+
+
+
 
 	}
 
 	@Override
 	public T dequeue() throws EmptyCollectionException
-	   {
-		
-		if (isEmpty()) {
+	{
+		Node<T> nodo;
+		if (front==null) {
 			throw new EmptyCollectionException("La cola esta vacia");
+		}else {
+			nodo = front;
+			front = front.next;
+
 		}
-		Node<T> nodo = front;
-		front = front.next;
-		count --;
-		if (isEmpty()) {
-			rear = null;
-		}
-		
-		
 		return nodo.element;
 
-	   }
+	}
 
 	@Override
 	public T first()  throws EmptyCollectionException{
-		if (isEmpty()) {
+		if (front ==null) {
 			throw new EmptyCollectionException("La cola esta vacia");
 		}
 		return front.element;
-	
-		
+
+
 	}
 
 	@Override
 	public boolean isEmpty() {
-		if (count == 0) {
+		if (front==null) {
 			return true;
+		}else {
+			return false;
 		}
-		return false;
 	}
 
 	@Override
 	public int size() {
-		
+		Node<T> actual = front;
+		while(actual != rear) {
+			count= count +1;
+			actual = actual.next;
+		}
 		return count;
 	}
 
 	@Override
 	public T dequeueLast() throws EmptyCollectionException {
-		if (isEmpty()) {
+		if (front==null) {
 			throw new EmptyCollectionException("La cola esta vacia");
 		}
-		Node<T> nodo = front;
+		Node<T> actual = front;
+		Node<T> nodo;//Nodo auxiliar para devolver los borrados
 		
-		if (nodo.next==null) {
+		//Para un solo elemento
+		if (actual.next==null) {
+			nodo=front;
 			front = null;
 			rear= null;
-			count --;
+			
 			return nodo.element;
 		}
+		//Mas de un elemento en la lista
+		while (actual!= null) {
+			if (actual.next.next==null) {
+				nodo = actual.next;
+				actual.next = null;
+				return nodo.element;
+			}
+			actual = actual.next;
 		
-		Node<T> auxiliar = null;
-		while (nodo!= null) {
-			auxiliar= nodo;
-			nodo= nodo.next;
-			
 		}
-		auxiliar.next=null;
-		count--;
 		
-		return nodo.element;
+		
+		return null;
+		
 	}
 
 	@Override
@@ -132,6 +144,6 @@ public class LinkedQueue<T> implements QueueADT<T> {
 		return ""; 
 
 
-};
+	};
 
 }
