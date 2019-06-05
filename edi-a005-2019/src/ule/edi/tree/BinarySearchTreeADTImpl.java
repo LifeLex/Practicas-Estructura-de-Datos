74485c4448ce88cc0ce0b3273989b13c717f7f8a
Alejandro <jandrito97@gmail.com>
2019-06-05 21:04:23 +0200

    Avance practica 5

--- edi-a005-2019/src/ule/edi/tree/BinarySearchTreeADTImpl.java
+++ edi-a005-2019/src/ule/edi/tree/BinarySearchTreeADTImpl.java
@@ -1,6 +1,7 @@
 package ule.edi.tree;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -58,7 +59,7 @@ import java.util.Stack;
  *            tipo de la información en cada nodo, comparable.
  */
 public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
-		AbstractBinaryTreeADT<T> {
+AbstractBinaryTreeADT<T> {
 
 	/**
 	 * Devuelve el árbol binario de búsqueda izquierdo.
@@ -73,7 +74,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	private void setLeftBST(BinarySearchTreeADTImpl<T> left) {
 		this.leftSubtree = left;
 	}
-	
+
 	/**
 	 * Devuelve el árbol binario de búsqueda derecho.
 	 */
@@ -84,14 +85,14 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	private void setRightBST(BinarySearchTreeADTImpl<T> right) {
 		this.rightSubtree = right;
 	}
-	
+
 	/**
 	 * Árbol BST vacío
 	 */
 	public BinarySearchTreeADTImpl() {
-		
+
 		setContent(null);
-		
+
 		setLeftBST(null);
 		setRightBST(null);
 	}
@@ -99,7 +100,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	private BinarySearchTreeADTImpl<T> emptyBST() {
 		return new BinarySearchTreeADTImpl<T>();
 	}
-	
+
 	/**
 	 * Inserta todos los elementos de una colección en el árbol.
 	 * 
@@ -110,7 +111,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 */
 	public void insert(Collection<T> elements) {
 		//	O todos o ninguno; si alguno es 'null', ni siquiera se comienza a insertar
-		//TODO Implementar el método
+
 		for (T i : elements) {
 			if (i == null) {
 				throw new IllegalArgumentException("Elemento nulo encontrado en la coleccion");
@@ -119,7 +120,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 		for (T j : elements) {
 			insert(j);
 		}
-		
+
 	}
 
 	/**
@@ -131,7 +132,7 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 */
 	public void insert(T ... elements) {
 		//	O todos o ninguno; si alguno es 'null', ni siquiera se comienza a insertar
-	    // TODO Implementar el método
+
 		for (int i = 0; i < elements.length; i++) {
 			if (elements[i] == null) {
 				throw new IllegalArgumentException("Elemento nulo encontrado en el array");
@@ -141,9 +142,9 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 		for (int i = 0; i < elements.length; i++) {
 			insert(elements[i]);
 		}
-		
+
 	}
-	
+
 	/**
 	 * Inserta de forma recursiva (como hoja) un nuevo elemento en el árbol de búsqueda.
 	 * 
@@ -168,25 +169,41 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 			this.getRightBST().insert(element);
 		}
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Elimina los elementos de la colección del árbol.
 	 */
 	public void withdraw(Collection<T> elements) {
-        //		O todos o ninguno; si alguno es 'null', no se eliminará ningún elemento
-	    // TODO Implementar el método
+		//		O todos o ninguno; si alguno es 'null', no se eliminará ningún elemento
+		for (T i : elements) {
+			if (i == null) {
+				throw new IllegalArgumentException("");
+			}
+		}
+
+		for (T t : elements) {
+			withdraw(t);
+		}
 	}
 
 	/**
 	 * Elimina los valores en un array del árbol.
 	 */
 	public void withdraw(T ... elements) {
-	    //		O todos o ninguno; si alguno es 'null', no se eliminará ningún elemento
-	    // TODO Implementar el método
+		//		O todos o ninguno; si alguno es 'null', no se eliminará ningún elemento
+		for (int i = 0; i < elements.length; i++) {
+			if (elements[i] == null) {
+				throw new IllegalArgumentException("Elemento nulo encontrado en el array");
+			}
+		}
+
+		for (int i = 0; i < elements.length; i++) {
+			withdraw(elements[i]);
+		}
 	}
-	
+
 	/**
 	 * Elimina un elemento del árbol.
 	 * 
@@ -194,9 +211,124 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 */
 	public void withdraw(T element) {
 		// 	Si el elemento tiene dos hijos, se tomará el criterio de sustituir el elemento por el mayor de sus menores y eliminar el mayor de los menores.
-		// TODO Implementar el método
+		//BORRAR RAIZ
+		if (content.equals(element)) {
+			if (isLeaf()) {
+				setContent(null);
+				setLeftBST(null);
+				setRightBST(null);
+			}else if (!isLeaf()) {
+				Collection<T> listaT = new ArrayList<T>();
+				T elemento = content;
+				int n=0;
+				listaT = raizCollection(listaT, elemento, n);
+				setContent(null);
+				setLeftBST(null);
+				setRightBST(null);
+				insert(listaT);
+			}
+		}
+		//BORRAR RAMA IZQ
+		if (this.getLeftBST().content!= null && this.getLeftBST().content.equals(element)) {
+			if (getLeftBST().isLeaf()) {
+				getLeftBST().setContent(null);
+				getLeftBST().setLeftBST(null);
+				getLeftBST().setRightBST(null);
+			}else if (!isLeaf()) {
+				Collection<T> listaT = new ArrayList<T>();
+				T elemento = content;
+				int n=0;
+				listaT = izquierdaCollection(listaT, elemento, n);
+				getLeftBST().setContent(null);
+				getLeftBST().setLeftBST(null);
+				getLeftBST().setRightBST(null);
+				insert(listaT);
+			}
+		}
+		//BORRAR RAMA DERECHA
+		if (this.getRightBST().content!= null && this.getRightBST().content.equals(element)) {
+			if (getRightBST().isLeaf()) {
+				getRightBST().setContent(null);
+				getRightBST().setLeftBST(null);
+				getRightBST().setRightBST(null);
+			}else if (!isLeaf()) {
+				Collection<T> listaT = new ArrayList<T>();
+				T elemento = content;
+				int n=0;
+				listaT = izquierdaCollection(listaT, elemento, n);
+				getRightBST().setContent(null);
+				getRightBST().setLeftBST(null);
+				getRightBST().setRightBST(null);
+				insert(listaT);
+			}
+		}
+
+
+		if (this.content.compareTo(element)>0) {
+			this.getLeftBST().withdraw(element);
+		}
+		if (this.content.compareTo(element)<0) {
+			this.getRightBST().withdraw(element);
+		}
+
+
+
 	}
-	
+	private Collection<T> raizCollection(Collection<T> listaT, T elemento, int n){
+		if (content!= null) {
+			if (content.compareTo(elemento)<0) {
+				n= n+1;
+				if (n>1) {
+					listaT.add(content);
+				}
+			}
+		}
+		if (getLeftBST() !=null) {
+			getLeftBST().raizCollection(listaT, elemento, n);
+		}
+		if (getRightBST() != null) {
+			getRightBST().raizCollection(listaT, elemento, n);
+		}
+		return listaT;
+
+
+	}
+	private Collection<T> izquierdaCollection(Collection<T> listaT, T elemento, int n){
+		if (content!= null) {
+			if (content.compareTo(elemento)<0) {
+				n= n+1;
+				if (n>1) {
+					listaT.add(content);
+				}
+			}
+		}
+		if (getLeftBST() !=null) {
+			getLeftBST().izquierdaCollection(listaT, elemento, n);
+		}
+		if (getRightBST() != null) {
+			getRightBST().izquierdaCollection(listaT, elemento, n);
+		}
+		return listaT;
+
+	}
+	private Collection<T> derechaCollection(Collection<T> listaT, T elemento, int n){
+		if (content!= null) {
+			if (content.compareTo(elemento)>0) {
+				n= n+1;
+				if (n>1) {
+					listaT.add(content);
+				}
+			}
+		}
+		if (getLeftBST() !=null) {
+			getLeftBST().derechaCollection(listaT, elemento, n);
+		}
+		if (getRightBST() != null) {
+			getRightBST().derechaCollection(listaT, elemento, n);
+		}
+		return listaT;
+
+	}
 	/**
 	 * Devuelve el sub-árbol indicado. (para tests)
 	 * path será el camino para obtener el sub-arbol. Está formado por 0 y 1.
@@ -204,9 +336,9 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 * "bajar por la derecha" como "1", el camino desde un 
 	 * nodo N hasta un nodo M (en uno de sus sub-árboles) será la
 	 * cadena de 0s y 1s que indica cómo llegar desde N hasta M.
-     *
-     * Se define también el camino vacío desde un nodo N hasta
-     * él mismo, como cadena vacía.
+	 *
+	 * Se define también el camino vacío desde un nodo N hasta
+	 * él mismo, como cadena vacía.
 	 * 
 	 * Si el subarbol no existe lanzará la excepción NoSuchElementException.
 	 * 
@@ -215,12 +347,44 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 * @throws NoSuchElementException si el subarbol no existe
 	 */
 	public BinarySearchTreeADTImpl<T> getSubtreeWithPath(String path) {
-		//TODO implementar el método
-		
-		return null;
-		
+		for (int i = 0; i < path.length(); i++) {
+			if (path.charAt(i) != '0' && path.charAt(i) != '1') {
+				throw new IllegalArgumentException("Illegal arguments");
+			}
+		}
+		return getSubtreeWithPathRecursivo(this, 0, path);
+
 	}	
-	
+
+
+	private BinarySearchTreeADTImpl<T> getSubtreeWithPathRecursivo(BinarySearchTreeADTImpl<T> nodo,
+			int i, String camino) {
+
+		if (nodo.content == null) {
+			throw new NoSuchElementException();
+		}
+
+		if (i > camino.length() - 1) {
+			return nodo;
+		}
+		if (camino.charAt(i) == '1') {
+			if (nodo.getRightBST().getContent() != null) {
+				i = i + 1;
+				return getSubtreeWithPathRecursivo(nodo.getRightBST(), i, camino);
+			} else {
+				throw new NoSuchElementException();
+			}
+		} else if (camino.charAt(i) == '0') {
+			if (nodo.getLeftBST().getContent() != null) {
+				i = i + 1;
+				return getSubtreeWithPathRecursivo(nodo.getLeftBST(), i, camino);
+			} else {
+				throw new NoSuchElementException();
+			}
+		}
+		return nodo;
+	}
+
 	/**
 	 * Acumula en orden descendente, una lista con los pares 'padre-hijo' en este árbol.
 	 * 
@@ -228,18 +392,18 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 * 
 	 * {10, {5, {2, ∅, ∅}, ∅}, {20, ∅, {30, ∅, ∅}}}
 	 * 
-     * 10
-     * |  5
-     * |  |  2
-     * |  |  |  ∅
-     * |  |  |  ∅
-     * |  |  ∅
-     * |  20
-     * |  |  ∅
-     * |  |  30
-     * |  |  |  ∅
-     * |  |  |  ∅
-     * 
+	 * 10
+	 * |  5
+	 * |  |  2
+	 * |  |  |  ∅
+	 * |  |  |  ∅
+	 * |  |  ∅
+	 * |  20
+	 * |  |  ∅
+	 * |  |  30
+	 * |  |  |  ∅
+	 * |  |  |  ∅
+	 * 
 	 * el resultado sería una lista de cadenas:
 	 * 
 	 * 	[(20,30), (10,20), (10,5), (5,2)]
@@ -252,83 +416,153 @@ public class BinarySearchTreeADTImpl<T extends Comparable<? super T>> extends
 	 * 
 	 * @param buffer lista con el resultado.
 	 */
+
+
 	public void parentChildPairsTagDescend(List<String> buffer) {
-	
-		// TODO Implementar el método
-	}
+		int descentN[] = { 0 };
 		
-	
-	
-	/**
-	 * Importante: Solamente se debe recorrer el árbol una vez
-	 * 
-	 * Comprueba si los elementos de la lista coinciden con algún camino desde la raiz.
-	 * Además, si existe algún camino que coincida con los elementos de la lista, los etiqueta en el árbol,
-	 * numerandolos empezando por la raiz como 1.
-	 * 
-	 * Por ejemplo, el árbol
-	 * 
-	 * {50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
-	 * 
-	 * si path = [50, 30, 10]
-	 * 
-	 * devolvería true y el árbol quedaría así etiquetado:
-	 * 
-	 * {50 [(path, 1)], {30 [(path, 2)], {10 [(path, 3)], ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
-	 * 
-	 * Para el mismo árbol, si path es [50, 40]  devolvería true y el árbol quedaría así etiquetado:
-	 * {50 [(path, 1)], {30, {10, ∅, ∅}, {40 [(path, 2)], ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
-	 * 
-	 * Para el mismo árbol, si path es [50, 80]  devolvería false y el árbol no se etiqueta:
-	 * {50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
-	 * 
-	 * 
-	 * @return  true si los elementos de la lista coinciden con algún camino desde la raiz,  falso si no es así
-	 */
-	public boolean isPathIn(List<T> path) {
-		//	TODO Implementar método
-		return false;
+		if(this.getRightBST().content!=null){
+			getRightBST().parentChildPairsTagDescend(buffer);
+			getRightBST().tagDescend(descentN);
+			//this.setTag("descend", descentN[0]);
+			buffer.add( "("+this.content+", "+this.getRightBST().content+")");
+			
+			
+		}
+		
+		descentN[0] += 1;
+		
+		this.setTag("descend", descentN[0]);
+		if(this.getLeftBST().content!=null){			
+			buffer.add( "("+this.content+", "+this.getLeftBST().content+")");
+			getLeftBST().parentChildPairsTagDescend( buffer);
+			getLeftBST().tagDescend(descentN);
+			
+		}
+		
+
+	//	System.out.println(buffer);
 	}
 
-	
-	/**
-	 * 
-	 * Etiqueta cada nodo con su posición en el recorrido en anchura, con la etiqueta "width"
-	 * 
-	 *  Por ejemplo, el árbol
-	 * 
-	 * {50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
-	 * 
-	 *  queda etiquetado como 
-	 * 
-	 *   {50 [(width, 1)], 
+
+
+
+
+	private void tagDescend(int[] descentN) {
+		
+		if (this.getRightBST().content != null) {
+			this.getRightBST().tagDescend(descentN);
+		}
+		descentN[0] += 1;
+		this.setTag("descend", descentN[0]);
+		if (this.getLeftBST().content != null) {
+			this.getLeftBST().tagDescend(descentN);
+			
+
+		}
+	}
+
+
+
+
+
+
+/**
+ * Importante: Solamente se debe recorrer el árbol una vez
+ * 
+ * Comprueba si los elementos de la lista coinciden con algún camino desde la raiz.
+ * Además, si existe algún camino que coincida con los elementos de la lista, los etiqueta en el árbol,
+ * numerandolos empezando por la raiz como 1.
+ * 
+ * Por ejemplo, el árbol
+ * 
+ * {50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
+ * 
+ * si path = [50, 30, 10]
+ * 
+ * devolvería true y el árbol quedaría así etiquetado:
+ * 
+ * {50 [(path, 1)], {30 [(path, 2)], {10 [(path, 3)], ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
+ * 
+ * Para el mismo árbol, si path es [50, 40]  devolvería false y el árbol no se etiqueta:
+ *{50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
+ * 
+ * Para el mismo árbol, si path es [50, 80]  devolvería true y el árbol quedaría así etiquetado:
+ * {50 [(path, 1)], {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80 [(path, 2)], {60, ∅, ∅}, ∅}}
+ * 
+ * 
+ * @return  true si los elementos de la lista coinciden con algún camino desde la raiz,  falso si no es así
+ */
+public boolean isPathIn(List<T> path) {
+	//	TODO Implementar método
+	return false;
+}
+
+
+/**
+ * 
+ * Etiqueta cada nodo con su posición en el recorrido en anchura, con la etiqueta "width"
+ * 
+ *  Por ejemplo, el árbol
+ * 
+ * {50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
+ * 
+ *  queda etiquetado como 
+ * 
+ *   {50 [(width, 1)], 
                  {30 [(width, 2)], {10 [(width, 4)], ∅, ∅},{40 [(width, 5)], ∅, ∅}},
                  {80 [(width, 3)], {60 [(width, 6)], ∅, ∅}, ∅}}
-	 * 
+ * 
 
-	 */	
-	public void tagWidth(){
-		//	TODO Implementar método
+ */	
+public void tagWidth(){
+	int [] width= {1};
+	String k = "width";
+	if (this.content==null) {
+		throw new NoSuchElementException();
 	}
-	
-	
-	/**	
-	 * Devuelve un iterador que recorre los elementos del arbol en inorden (de menor a mayor)
-	 * 
-	 * Por ejemplo, con el árbol
-	 * 
-	 * 		{50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
-	 * 
-	 * y devolvería el iterador que recorrería los ndos en el orden: 10, 30, 40, 50, 60, 80
-	 * 
-	 * 		
-	 * 
-	 * @return iterador para el recorrido inorden o ascendente
-	 */
-	public Iterator<T> iteratorInorden() {
-		//	TODO Implementar método
-		return null;
-	}	
-	
+	this.setTag(k, width[0]++);
+
+	tagWidthRec(width);
+}
+
+
+private void tagWidthRec(int[] width) {
+	String k = "width";
+	if (this.getLeftBST().content!=null ) {
+		this.getLeftBST().setTag(k, width[0]++);
+	}
+	if (this.getRightBST().content!=null) {
+		this.getRightBST().setTag(k,width[0]++);
+	}
+	if (this.getLeftBST().content!=null) {
+		this.getLeftBST().tagWidthRec(width);
+	}
+	if (this.getRightBST().content!=null) {
+
+		this.getRightBST().tagWidthRec(width);
+	}
+
+
+}
+
+/**	
+ * Devuelve un iterador que recorre los elementos del arbol en inorden (de menor a mayor)
+ * 
+ * Por ejemplo, con el árbol
+ * 
+ * 		{50, {30, {10, ∅, ∅}, {40, ∅, ∅}}, {80, {60, ∅, ∅}, ∅}}
+ * 
+ * y devolvería el iterador que recorrería los ndos en el orden: 10, 30, 40, 50, 60, 80
+ * 
+ * 		
+ * 
+ * @return iterador para el recorrido inorden o ascendente
+ */
+public Iterator<T> iteratorInorden() {
+	//	TODO Implementar método
+	return null;
+}	
+
 }
 
--- edi-a005-2019/src/ule/edi/tree/BinarySearchTreeADTTests.java
+++ edi-a005-2019/src/ule/edi/tree/BinarySearchTreeADTTests.java
@@ -1,13 +1,14 @@
 package ule.edi.tree;
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.NoSuchElementException;
 
 
 
@@ -125,6 +126,7 @@ public class BinarySearchTreeADTTests {
 		@Test
 		public void testTagDescendTC4() {
 			List<String> lista= new LinkedList<String>();
+			//TEx.parentChildPairsTagDescend(lista);
 			TC3.parentChildPairsTagDescend(lista);
 			Assert.assertEquals(lista.toString(), "[(80, 90), (80, 70), (50, 80), (50, 20), (20, 30), (20, 10)]");
 			TC3.filterTags("descend");
@@ -132,6 +134,104 @@ public class BinarySearchTreeADTTests {
 			
 		}
 	
+		@Test
+		public void getSubtreeWithPathTest() {
+
+			TE.insert(17);
+			TE.insert(22);
+			TE.insert(19);
+			TE.insert(4);
+			TE.insert(10);
+			TE.insert(18);
+			TE.insert(30);
+			TE.insert(1);
+			Assert.assertEquals("{18, ∅, ∅}", TE.getSubtreeWithPath("100").toString());
+		}
+	
+
+		@Test(expected = NoSuchElementException.class)
+		public void getSubtreeWithPath2Test() {
+
+			TC3.getSubtreeWithPath("111111");
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		public void getSubtreeWithPath3Test() {
+
+			TC3.getSubtreeWithPath("84848");
+		}
+
+		@Test
+		public void insertTest() {
+
+			TE.insert(17);
+			TE.insert(22);
+			TE.insert(19);
+			TE.insert(4);
+			TE.insert(10);
+			TE.insert(18);
+			TE.insert(30);
+			TE.insert(1);
+			// System.out.println(TE.toString());
+			Assert.assertEquals("{17, {4, {1, ∅, ∅}, {10, ∅, ∅}}, {22, {19, {18, ∅, ∅}, ∅}, {30, ∅, ∅}}}",
+					TE.toString());
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		public void insertExcepcionTest() {
+
+			TE.insert(17);
+			TE.insert(22);
+			TE.insert(19);
+			TE.insert(4);
+			TE.insert(10);
+			TE.insert(18);
+			TE.insert(30);
+			TE.insert(1);
+			Integer a = null;
+			TE.insert(a);
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		public void insertExcepcionDTest() {
+
+			TE.insert(17, 22, 19, 4, 10, 18, 30, 1);
+			Integer a = null;
+			TE.insert(a);
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		public void insertExcepcionETest() {
+
+			TE.insert(17, null, 22, 19, 4, 10, 18, 30, 1);
+		}
+
+		@Test
+		public void insertExceptionFTest() {
+
+			ArrayList<Integer> lista = new ArrayList<Integer>();
+			lista.add(10);
+			TE.insert(lista);
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		public void insertExceptionGTest() {
+
+			ArrayList<Integer> lista = new ArrayList<Integer>();
+			lista.add(null);
+			TE.insert(lista);
+		}
+		
+		@Test
+		public void tagWidthTest() {
+			TV1.tagWidth();
+			Assert.assertEquals("{10 [(width, 1)], {5 [(width, 2)], ∅, {7 [(width, 4)], {6 [(width, 5)], ∅, ∅}, ∅}}, {15 [(width, 3)], ∅, ∅}}" , TV1.toString());
+		}
+		@Test(expected = NoSuchElementException.class)
+		public void tagWidthTest2() {
+			TE.tagWidth();;
+		}
+		
 	}
 
 
